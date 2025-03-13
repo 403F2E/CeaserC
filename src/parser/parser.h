6010d185cd0fe79f7882e9eb90c4b8fc276bec2a
BUSHIWAY <99516593+BUSHIWAY@users.noreply.github.com>
2025-03-13 10:30:52 +0000

    feat: adding some enums to the parser

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -2,3 +2,63 @@
 
 #pragma once
 #include "../../headers/utils.h"
+
+typedef enum {
+  CONST,
+  INT,
+  BIN,
+  OCT,
+  HEX,
+  FLOAT,
+  DOUBLE,
+  CHAR,
+  BOOL,
+  KEY,
+  CIPHER,
+} PARSED_TYPES;
+
+typedef enum {
+  IDENT,
+  DEF,
+  CLASS,
+  ALGO,
+  RETURN,
+  IF,
+  ELSE,
+  FOR,
+  WHILE,
+  DO,
+  BREAK,
+  CONTINUE,
+  MATCH,
+  DEFAULT,
+} PARSED_KEYWORD;
+
+typedef enum {
+  LT,
+  LE,
+  EQ,
+  NE,
+  GE,
+  GT,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+  ASSIGN,
+  INC,
+  DEC,
+  POWER,
+  LOGICAL_AND,
+  LOGICAL_OR,
+  LOGICAL_NOT,
+  BITWISE_AND,
+  BITWISE_OR,
+  BITWISE_XOR,
+  BITWISE_NOT,
+  BITWISE_SHL,
+  BITWISE_RSHL,
+} PARSED_SYMBOLS;
+
+void parser();
